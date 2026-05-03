0b91ac4662eb19965b1dae0159f9fc1b1870b9fc
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-05-03 18:05:10 +0530

    Add solution for hinge-loss

---
+++ hinge-loss/hinge-loss.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def hinge_loss(y_true, y_score, margin=1.0, reduction="mean") -> float:
+    """
+    y_true: 1D array of {-1,+1}
+    y_score: 1D array of real scores, same shape as y_true
+    reduction: "mean" or "sum"
+    Return: float
+    """
+    # Write code here
+    y_true = np.asarray(y_true)
+    y_score = np.array(y_score)
+    loss = np.maximum(0, margin - y_true * y_score)
+    ans = 0
+    match reduction:
+        case "mean":
+            ans = np.mean(loss)
+        case "sum":
+            ans = np.sum(loss)
+    return ans
+        
