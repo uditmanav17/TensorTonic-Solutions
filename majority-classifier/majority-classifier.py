2efd43daab7864dbdf4c78a3585400088b117c3d
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-05-25 21:25:23 +0530

    Add solution for majority-classifier

---
+++ majority-classifier/majority-classifier.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def majority_classifier(y_train, X_test):
+    """
+    Predict the most frequent label in training data for all test samples.
+    """
+    # Write code here
+    chars, counts = np.unique(y_train, return_counts=True)
+    max_idx = np.argmax(counts)
+    # print(chars, counts, max_idx)
+    return np.array([chars[max_idx]] * len(X_test))
