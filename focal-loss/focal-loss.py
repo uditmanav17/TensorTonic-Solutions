3f5419f422c9af845ba7c0f9daea69579d22e532
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-05-28 12:00:32 +0530

    Add solution for focal-loss

---
+++ focal-loss/focal-loss.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def focal_loss(p, y, gamma=2.0):
+    """
+    Compute Focal Loss for binary classification.
+    """
+    # Write code here
+    p = np.array(p)
+    y = np.array(y)
+    loss = (
+        - np.pow(1 - p, gamma) * y * np.log(p) 
+        - np.pow(p, gamma) * (1 - y) * np.log(1 - p)
+    )
+    # print(loss.mean())
+    return loss.mean()
