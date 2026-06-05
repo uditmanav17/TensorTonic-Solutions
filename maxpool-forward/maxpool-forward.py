9e5f33518c9c912cc4fbcca72dceda6d530657c9
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-06-05 10:00:00 +0530

    Update solution for maxpool-forward

--- maxpool-forward/maxpool-forward.py
+++ maxpool-forward/maxpool-forward.py
@@ -9,14 +9,19 @@ def maxpool_forward(X, pool_size, stride):
     # Write code here
     X = np.array(X)
     in_rows, in_cols = X.shape
-    out_rows = math.floor((in_rows - pool_size) / stride) + 1
-    out_cols = math.floor((in_cols - pool_size) / stride) + 1
-    out = np.zeros(shape=(out_rows, out_cols))
+    out_rows = (in_rows - pool_size) // stride + 1
+    out_cols = (in_cols - pool_size) // stride + 1
+    out = np.empty(shape=(out_rows, out_cols))
     for i, j in product(range(out_rows), range(out_cols)):
         # print(i, j)
         # print(f"rslice = {i*stride} - {i*stride + pool_size}")
         # print(f"cslice = {j*stride} - {j*stride + pool_size}")
         # print(X[i*stride: i*stride + pool_size, j*stride: j*stride + pool_size])
-        out[i][j] = np.max(X[i*stride: i*stride + pool_size, j*stride: j*stride + pool_size])
+        out[i, j] = np.max(
+            X[
+                i*stride: i*stride + pool_size, 
+                j*stride: j*stride + pool_size
+            ]
+        )
     # print(out)
     return out.tolist()
