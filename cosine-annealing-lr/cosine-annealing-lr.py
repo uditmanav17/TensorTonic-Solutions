fc9bd18b9964c35b972494a1fe400d9325624c67
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-06-24 10:39:45 +0530

    Solve cosine-annealing-lr

--- cosine-annealing-lr/cosine-annealing-lr.py
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -1,9 +1,6 @@
-import math
-
 def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
     """
     Compute the learning rate using cosine annealing.
     """
-    # Write code here
     term = 0.5 * (base_lr - min_lr) * (1 + math.cos(math.pi * current_step / total_steps))
     return min_lr + term
