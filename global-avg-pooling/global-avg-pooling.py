976904e9dc5f769bf385a232122d5260bbf6e00d
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-06-30 09:53:56 +0530

    Solve global-avg-pooling

--- global-avg-pooling/global-avg-pooling.py
+++ global-avg-pooling/global-avg-pooling.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-
-
 def global_avg_pool(x):
     """
     Compute global average pooling over spatial dims.
     Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
     """
-    # Write code here
     if x.ndim < 3:
         raise ValueError("Input must have at least 3 dimensions")
 
-    return np.mean(x, axis=tuple(range(x.ndim - 2, x.ndim)))
+    axis = tuple(range(x.ndim - 2, x.ndim))
+    # print(axis)
+    return np.mean(x, axis=axis)
