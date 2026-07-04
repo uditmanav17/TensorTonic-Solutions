4e3abd2ec04dd9d702413fa33de212c1271128ab
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-07-04 10:23:54 +0530

    Solve ridge-regression

--- ridge-regression/ridge-regression.py
+++ ridge-regression/ridge-regression.py
@@ -4,7 +4,6 @@ def ridge_regression(X, y, lam):
     """
     Compute ridge regression weights using the closed-form solution.
     """
-    # Write code here
     X = np.array(X)
     y = np.array(y)
     
