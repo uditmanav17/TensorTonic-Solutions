f4b8f3ed1d30c97cabb221fa3cbf85dd4f9ab5da
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-07-10 09:20:03 +0530

    Solve maxpool-forward

--- maxpool-forward/maxpool-forward.py
+++ maxpool-forward/maxpool-forward.py
@@ -6,22 +6,16 @@ def maxpool_forward(X, pool_size, stride):
     """
     Compute the forward pass of 2D max pooling.
     """
-    # Write code here
     X = np.array(X)
     in_rows, in_cols = X.shape
     out_rows = (in_rows - pool_size) // stride + 1
     out_cols = (in_cols - pool_size) // stride + 1
     out = np.empty(shape=(out_rows, out_cols))
     for i, j in product(range(out_rows), range(out_cols)):
-        # print(i, j)
-        # print(f"rslice = {i*stride} - {i*stride + pool_size}")
-        # print(f"cslice = {j*stride} - {j*stride + pool_size}")
-        # print(X[i*stride: i*stride + pool_size, j*stride: j*stride + pool_size])
         out[i, j] = np.max(
             X[
                 i*stride: i*stride + pool_size, 
                 j*stride: j*stride + pool_size
             ]
         )
-    # print(out)
     return out.tolist()
