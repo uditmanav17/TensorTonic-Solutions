f591201bd46b98f94556fc7e7d021f936e650fe9
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-07-13 10:05:08 +0530

    Solve image-histogram

--- image-histogram/image-histogram.py
+++ image-histogram/image-histogram.py
@@ -4,7 +4,6 @@ def image_histogram(image):
     """
     Compute the intensity histogram of a grayscale image.
     """
-    # Write code here
     arr = [0] * 256
     for val in chain.from_iterable(image):
         arr[val] += 1
