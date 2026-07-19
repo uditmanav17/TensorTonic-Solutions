f58effde1503da25a29a8412554bd89462d22077
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-07-19 11:12:45 +0530

    Solve ridge-regression

--- ridge-regression/ridge-regression.py
+++ ridge-regression/ridge-regression.py
@@ -4,8 +4,8 @@ def ridge_regression(X, y, lam):
     """
     Compute ridge regression weights using the closed-form solution.
     """
-    X = np.array(X)
-    y = np.array(y)
+    X = np.asarray(X)
+    y = np.asarray(y)
     
     return np.linalg.inv(X.T @ X + lam * np.eye(X.shape[1])) @ X.T @ y
     
