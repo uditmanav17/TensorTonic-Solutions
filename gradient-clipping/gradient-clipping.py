c588586492b8264303341186358505ecf3a979fc
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-07-27 14:39:59 +0530

    Solve gradient-clipping

--- gradient-clipping/gradient-clipping.py
+++ gradient-clipping/gradient-clipping.py
@@ -4,7 +4,6 @@ def clip_gradients(g, max_norm):
     """
     Clip gradients using global norm clipping.
     """
-    # Write code here
     g = np.array(g)
     g_norm = np.linalg.norm(g)
     if g_norm <= max_norm or max_norm <= 0:
