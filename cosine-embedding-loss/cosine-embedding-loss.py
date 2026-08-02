e662f16b7647a8a5e88c0e7d034f9b595d15c75a
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-08-02 09:22:28 +0530

    Solve cosine-embedding-loss

--- cosine-embedding-loss/cosine-embedding-loss.py
+++ cosine-embedding-loss/cosine-embedding-loss.py
@@ -4,10 +4,15 @@ def cosine_embedding_loss(x1, x2, label, margin):
     """
     Compute cosine embedding loss for a pair of vectors.
     """
-    # Write code here
-    cos_x1x2 = np.dot(x1, x2) / (np.linalg.norm(x1) * np.linalg.norm(x2))
+    x1 = np.asarray(x1)
+    x2 = np.asarray(x2)
+
+    cos_sim = x1 @ x2 / (np.linalg.norm(x1) * np.linalg.norm(x2))
+
     if label == 1:
-        loss = 1 - cos_x1x2
-    if label == -1:
-        loss = max(0, cos_x1x2 - margin)
+        loss = 1 - cos_sim
+    else:
+        loss = max(0, cos_sim - margin)
+
     return loss
+    
