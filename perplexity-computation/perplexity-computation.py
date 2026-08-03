1592ccdbbf967e24672747640e91d5cc6aa6c0de
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-08-03 10:25:18 +0530

    Solve perplexity-computation

--- perplexity-computation/perplexity-computation.py
+++ perplexity-computation/perplexity-computation.py
@@ -4,9 +4,9 @@ def perplexity(prob_distributions, actual_tokens):
     """
     Compute the perplexity of a token sequence given predicted distributions.
     """
-    H = 0
-    for idx, tkn in enumerate(actual_tokens):
-        H += np.log(prob_distributions[idx][tkn])
-    H = -H / len(actual_tokens)
-    return np.exp(H)
+    prob_distributions = np.asarray(prob_distributions)
+    actual_tokens = np.asarray(actual_tokens)
+
+    probs = prob_distributions[np.arange(actual_tokens.size), actual_tokens]
+    return np.exp(-np.mean(np.log(probs)))
         
