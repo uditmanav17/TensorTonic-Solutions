06977d5811ddbcbce9f68b41dfc7e4d271f6d87f
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-08-06 09:49:03 +0530

    Solve precision-recall-at-k

--- precision-recall-at-k/precision-recall-at-k.py
+++ precision-recall-at-k/precision-recall-at-k.py
@@ -2,7 +2,6 @@ def precision_recall_at_k(recommended, relevant, k):
     """
     Compute precision@k and recall@k for a recommendation list.
     """
-    # Write code here
     top_k = set(recommended[:k])
     precison_k = len(top_k.intersection(relevant)) / k
     recall_k = len(top_k.intersection(relevant)) / len(relevant)
