ce48b1a1cfa04a977e5ebc047492e11758d43c61
Udit Manav <17214595+uditmanav17@users.noreply.github.com>
2026-09-01 13:34:18 +0530

    Solve max-pooling-2d

--- max-pooling-2d/max-pooling-2d.py
+++ max-pooling-2d/max-pooling-2d.py
@@ -1,24 +1,20 @@
+import numpy as np
+
 def max_pooling_2d(X: list, pool_size: int) -> list:
     """
     Returns non-overlapping maximum-pooled windows.
     """
-    rows = len(X)
-    cols = len(X[0])
+    X = np.asarray(X)
 
-    result = []
+    h, w = X.shape
+    h_out = h // pool_size
+    w_out = w // pool_size
 
-    for i in range(0, rows - pool_size + 1, pool_size):
-        row = []
+    X = X[:h_out * pool_size, :w_out * pool_size]
 
-        for j in range(0, cols - pool_size + 1, pool_size):
-            window_max = max(
-                X[r][c]
-                for r in range(i, i + pool_size)
-                for c in range(j, j + pool_size)
-            )
+    X = X.reshape(
+        h_out, pool_size,
+        w_out, pool_size
+    )
 
-            row.append(window_max)
-
-        result.append(row)
-
-    return result
+    return X.max(axis=(1, 3)).tolist()
